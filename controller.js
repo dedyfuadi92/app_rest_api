033052a511a1cbd18b47b3d9e84cf782e77280b9
dedyfuadi92 <dedyfuadi92@gmail.com>
2021-12-21 11:07:51 +0800

    tampilkan semua data

--- controller.js
+++ controller.js
@@ -4,3 +4,12 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok('Aplikasi rest api is worked', res);
 };
+exports.tampilSemuaData = function (req, res) {
+    connection.query('select *from mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
